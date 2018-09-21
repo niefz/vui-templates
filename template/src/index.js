5fb4ced85af5eb02563f0e22887d6b7c4d1c37db
niefz <fengzhao1.nie@midea.com>
2018-09-21 18:45:16 +0800

    update

--- template/src/index.js
+++ template/src/index.js
@@ -7,27 +7,26 @@ import Vue from 'vue';
 {{#router}}
 import VueRouter from 'vue-router';
 {{/router}}
-
 {{#UI}}
+
 {{#if_eq UIConfig 'element-ui'}}
 import { Loading } from 'element-ui';
 {{/if_eq}}
 {{/UI}}
-
 {{#router}}
+
 import Router from './routers';
 {{/router}}
 {{#vuex}}
 import store from './stores';
 {{/vuex}}
-
 {{#i18n}}
+
 import { i18n, loadLanguageAsync } from './i18n';
 {{/i18n}}
 
 import './styles/reset.scss';
 import './styles/common.scss';
-
 {{#UI}}
 {{#if_eq UIConfig 'element-ui'}}
 import './styles/element.scss';
@@ -39,18 +38,16 @@ import App from './index.vue';
 {{#router}}
 Vue.use(VueRouter);
 {{/router}}
-
 {{#UI}}
+
 {{#if_eq UIConfig 'element-ui'}}
 Vue.use(Loading.directive);
 {{/if_eq}}
 {{/UI}}
-
-
 {{#router}}
+
 const router = new VueRouter(Router);
 {{/router}}
-
 {{#i18n}}
 const lang = 'zh';
 loadLanguageAsync(lang);
