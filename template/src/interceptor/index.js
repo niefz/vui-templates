04fe17741b5bcfe147713970411901a02de34706
niefz <fengzhao1.nie@midea.com>
2018-09-26 14:28:22 +0800

    update

--- template/src/interceptor/index.js
+++ template/src/interceptor/index.js
@@ -6,17 +6,36 @@ import { Message } from 'element-ui';
 
 axios.defaults.baseURL = '/';
 
-axios.interceptors.response.use((res) => {
-  if (res && res.data && res.data.code) {
-    if (res.data.code !== '0') {
-      Message({
-        type: 'error',
-        message: res.data.msg,
-        duration: '1500',
-      });
+axios.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+)
+
+axios.interceptors.response.use(
+  (res) => {
+    if (res && res.data && res.data.code) {
+      if (res.data.code !== '0') {
+        Message({
+          type: 'error',
+          message: res.data.msg,
+          duration: '1500',
+        });
+      }
     }
-  }
-  return res;
-});
+    return res;
+  },
+  (err) => {
+    Message({
+      type: 'error',
+      message: err,
+      duration: '1500',
+    });
+    return Promise.reject(err)
+  },
+);
 
 export default axios;
