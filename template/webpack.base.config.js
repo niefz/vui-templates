ec9d1478bc248193f3efdfe1eaa25e4116e04ae9
niefz <fengzhao1.nie@midea.com>
2018-09-26 11:14:32 +0800

    update

--- template/webpack.base.config.js
+++ template/webpack.base.config.js
@@ -141,7 +141,7 @@ module.exports = webpackMerge({
     ]
   },
   resolve: {
-    extensions: ['.vue', '.js', '.scss', '.less'],
+    extensions: ['.vue', '.js', '.scss'],
     alias: {
       'vue': 'vue/dist/vue.js',
       'src': APP_PATH
