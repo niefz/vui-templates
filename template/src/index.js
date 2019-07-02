b135ad3f68bd87a5ada6aedc8f0fb957f8b79157
niefz <fengzhao1.nie@midea.com>
2019-07-02 16:06:58 +0800

    eslint

--- template/src/index.js
+++ template/src/index.js
@@ -41,9 +41,9 @@ const app = new Vue({
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
       console.log('SW registered: ', registration);
-    }).catch(registrationError => {
+    }).catch((registrationError) => {
       console.log('SW registration failed: ', registrationError);
     });
   });
